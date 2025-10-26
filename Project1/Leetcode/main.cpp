560a6cc90609474168b3105148a44d7efed89acd
Git-Mere <meregi7410@gmail.com>
2025-10-26 15:10:54 -0700

    numbers of 1 bits and add .cpp each problem

--- Project1/Leetcode/main.cpp
+++ Project1/Leetcode/main.cpp
@@ -1,30 +1,6 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <unordered_set>
-#include <queue>
-
-using namespace std;
-
-
-int getSum(int a, int b) {
-    int xor_v = a ^ b;
-    int and_v = a & b;
-
-    if (and_v == 0) {
-        return xor_v;
-    }
-    else if (xor_v == 0) {
-        return and_v << 1;
-    }
-
-    and_v <<= 1;
-
-    return getSum(xor_v, and_v);
-}
-
+#include "Control_Header.h"
 
 int main() {
-    getSum(1, 2);
+    Binary::getSum(1, 2);
     return 0;
 }
