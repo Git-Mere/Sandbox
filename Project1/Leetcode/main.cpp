85db92afb72f5248785eacf1935a82f89dd6e64b
Git-Mere <meregi7410@gmail.com>
2025-10-25 18:35:51 -0700

    binary add

--- Project1/Leetcode/main.cpp
+++ Project1/Leetcode/main.cpp
@@ -7,46 +7,24 @@
 using namespace std;
 
 
-vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>> result;
-    int size = nums.size();
-    sort(nums.begin(), nums.end());
-    int sum = 0;
+int getSum(int a, int b) {
+    int xor_v = a ^ b;
+    int and_v = a & b;
 
-    for (int i = 0; i < size - 1; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) {
-            continue;
-        }
-        int j = size - 1;
-        int k = i + 1;
-        if ((nums[i] < 0 && nums[j] < 0) || (nums[i] > 0 && nums[j] > 0)) { break; }
-
-        while (j != k) {
-            sum = nums[i] + nums[j] + nums[k];
-            if (sum == 0) {
-                result.push_back({ nums[i], nums[j], nums[k] });
-                while (nums[k] == nums[k - 1] && k < j) {
-                    k++;
-                }
-            }
-            else if (sum > 0) { j--; }
-            else if (sum < 0) { k++; }
-        }
-
+    if (and_v == 0) {
+        return xor_v;
+    }
+    else if (xor_v == 0) {
+        return and_v << 1;
     }
 
-    return result;
+    and_v <<= 1;
+
+    return getSum(xor_v, and_v);
 }
 
 
 int main() {
-    std::vector<int> test{ 1,2 };
-    for (int i = 0; i < test.size(); i++) {
-        std::cout << i << std::endl;
-        test.pop_back();
-    }
-
-    std::vector<int> limits = { 0,0,0,0 };
-    threeSum(limits);
+    getSum(1, 2);
     return 0;
 }
